1bbbe798cdc987010825a7348fb9b8839e716548
c <septembercult@gmail.com>
2024-10-20 22:19:00 +0300

    Fixed keyboard agent settings.

--- agents/keyboard_agent.py
+++ agents/keyboard_agent.py
@@ -8,12 +8,12 @@ class KeyboardAgent:
         self.settings = json.load(f)
         f.close()
 
-        self.activate_macros_key_cap = self.settings['activateMacrosKeyCap']
+        self.activate_macros_key_cap = f'{self.settings['activateMacrosKeyCap']}'
         self.capture_process = None
 
     def start_keyboard_listening(self):
         def on_keyboard_click(key):
-            if key == keyboard.Key.up:
+            if key.char == self.activate_macros_key_cap:
                 winsound.Beep(frequency=2500, duration=10)
                 #self.is_activated = not self.is_activated
 
