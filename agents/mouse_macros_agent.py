a1abedaf2be00178481aca827de08ee46ce26b59
c <septembercult@gmail.com>
2024-10-20 21:48:58 +0300

    Added keyboard listener.

--- agents/mouse_macros_agent.py
+++ agents/mouse_macros_agent.py
@@ -1,31 +1,48 @@
-from pynput import mouse
-import pyautogui
+import json
+from pynput import mouse, keyboard
 import time
+import winsound
 
 class MouseMacrosAgent:
     def __init__(self) -> None:
-        self.is_button_held = False
+        f = open('./appsettings.json')
+        self.settings = json.load(f)
+        f.close()
+
+        self.activate_macros_key_cap = self.settings['activateMacrosKeyCap']
+
         self.capture_process = None
+        self.is_activated = True
+        self.is_button_held = False
 
     def start_mouse_macros(self):
-        def on_click(x, y, button, pressed):
-            if button == mouse.Button.right:  # Check for the left mouse button
+        def on_keyboard_click(key):
+            if key == keyboard.Key.up:
+                winsound.Beep(frequency=2500, duration=10)
+                self.is_activated = not self.is_activated
+
+        keyboard_listener = keyboard.Listener(on_press=on_keyboard_click) # Если привести к вид как у mouse_listener, он как-то начинает работать.
+
+        def on_mouse_click(x, y, button, pressed):
+            if button == mouse.Button.right:
                 self.is_button_held = pressed
-                if pressed:
+                if self.is_button_held:
                     print("Right mouse button pressed.")
                 else:
                     print("Right mouse button released.")
 
-        with mouse.Listener(on_click=on_click) as listener:
+        with mouse.Listener(on_click=on_mouse_click) as mouse_listener:
             #pyautogui.moveRel(1, 10)
             print("Listening for mouse events. Press Ctrl+C to stop.")
+
             try:
                 while True:
-                    if self.is_button_held:
+                    if self.is_activated and self.is_button_held:
                         print("Right mouse button is being held down.")
                     time.sleep(0.1)  # Check every 100 ms
             except KeyboardInterrupt:
                 print("Stopped listening.")
-                listener.stop()
+                mouse_listener.stop()
 
-        listener.join()
+        keyboard_listener.join()
+        mouse_listener.join()
